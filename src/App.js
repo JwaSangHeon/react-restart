73d9f511544f5686845f5927c415886e3e4b809c
JwaSangHeon <whk4051@naver.com>
2022-11-10 17:54:03 +0900

    배열에 값 추가, concat 또는 spread 문법 사용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import React from "react";
-import Hello from "./components/Hello";
+import React, { useRef, useState } from "react";
 import Wrapper from "./components/Wrapper";
-import Counter from "./components/Counter";
-import InputSample from "./components/InputSample";
 import UserList from "./components/UserList";
+import CreateUser from "./components/CreateUser";
 
 function App() {
-  const name = "SH";
-  const color = "skyblue";
+  const [users, setUsers] = useState([
+    { id: 1, username: "SH", email: "SH@example.com" },
+    { id: 2, username: "JH", email: "HH@example.com" },
+    { id: 3, username: "SY", email: "SY@example.com" },
+  ]);
+
+  const nextId = useRef(4);
+
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = user;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+    setUser({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
   return (
     <Wrapper>
-      {/* <Hello name={name} color={color} isStar />
-      <Counter />
-      <InputSample /> */}
-      <UserList />
+      <CreateUser
+        onChange={onChange}
+        username={username}
+        email={email}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
     </Wrapper>
   );
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,13 +1,7 @@
 import React from "react";
 import User from "./User";
 
-const UserList = () => {
-  const users = [
-    { id: 1, username: "SH", email: "SH@example.com" },
-    { id: 2, username: "JH", email: "HH@example.com" },
-    { id: 3, username: "SY", email: "SY@example.com" },
-  ];
-
+const UserList = ({ users }) => {
   return (
     <>
       <User users={users} />
