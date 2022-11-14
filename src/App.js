fc742ce9172af425eebb207ffee611f841fc5554
JwaSangHeon <whk4051@naver.com>
2022-11-14 13:52:19 +0900

    params 이용

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import Profile from "./components/Profile";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
       </Routes>
     </>
   );
